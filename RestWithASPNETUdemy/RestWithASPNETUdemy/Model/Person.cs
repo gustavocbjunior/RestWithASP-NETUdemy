5f79bea269e0cd8cf090ea03c0bb7d3425edf704
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-01-13 01:20:20 -0300

    test

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -11,6 +11,6 @@ namespace RestWithASPNETUdemy.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-        public string Gender { get; set; }
+        public string Gender { get; set; } 
     }
 }
