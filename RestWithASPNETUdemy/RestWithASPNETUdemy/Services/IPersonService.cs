17a7eb1d49124b831366c21aa69adf344b98afab
gustavocbjunior <gustavobarbosaa@gmail.com>
2019-01-18 21:07:24 -0300

    create interface IPersonService

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        Person FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
